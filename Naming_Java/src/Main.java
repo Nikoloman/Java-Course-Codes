892bd9f6df71fc583386b4c12e2fc8e7f86f9e82
Nikoloman <hectorvel52@gmail.com>
2022-12-23 15:28:56 -0600

    A code making examples about naming variables in Java and ays to do it

---
+++ Naming_Java/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //In Java, we can't use kebab-case since we can't use "-" in the naming
+        //We can use any letter or character to name our variables unless that
+        //character is a number or an operator in Java
+        //We can use any letter that the language can read, "$" and "_" to name our
+        //variables in Java
+
+        //PascalCase
+        int OddNumber = 5;
+        int PrimeNumber = 11;
+        int NegativeNumber = -20;
+
+        //camelCase
+        int oddNumber = 3;
+        int primeNumber = 7;
+        int negativeNumber = -3;
+
+        //snake_case
+        int odd_number = 9;
+        int prime_number = 13;
+        int negative_number = -69;
+    }
+}
