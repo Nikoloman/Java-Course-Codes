433cc59905eb4385d43fb115de5945fc2763edbb
Nikoloman <hectorvel52@gmail.com>
2022-12-23 11:51:29 -0600

    A new practice program about Variables in Java

---
+++ Variables/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Declaring the variable
+        int number;
+        //Giving a value to our variable
+        number = 10;
+        System.out.println(number);
+
+        //Declaring the variable at the same time we give a value
+        int salary = 1000;
+        String employeeName = "Héctor Velázquez";
+        //Printing both variables
+        System.out.println("The employee " + employeeName + " has a salary of " + salary);
+    }
+}
