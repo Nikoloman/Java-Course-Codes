09c6e621e49cd7252da6900e042c9bf446444d93
Nikoloman <hectorvel52@gmail.com>
2022-12-26 17:53:22 -0600

    A practice using If Else

---
+++ If_Statement/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Write a number");
+        int num1 = in.nextInt();
+
+        if (num1 > 0){
+            System.out.println("The number is positive");
+        }
+        else{
+            if (num1 < 0){
+                System.out.println("The number is negative");
+            }
+            else{
+                System.out.println("The number is zero");
+            }
+        }
+    }
+}
