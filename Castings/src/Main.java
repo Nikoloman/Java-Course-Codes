18f8f2ca87fa7cbfe6c98c30c7eb73bf33e9aac6
Nikoloman <hectorvel52@gmail.com>
2022-12-26 15:24:08 -0600

    A practice with casting and changing types of values

---
+++ Castings/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 10.56;
+        double num2 = 3.12;
+
+        double num3 = num1 + num2;
+        System.out.println(num3);
+
+        int num4 = (int) num3;
+        System.out.println(num4);
+    }
+}
