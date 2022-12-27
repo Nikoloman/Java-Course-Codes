627d5cbfcc4107e0149bd1ab0652ba8c4928161d
Nikoloman <hectorvel52@gmail.com>
2022-12-27 13:55:39 -0600

    A little practice about Logic operations and stuff

---
+++ Logic-Operations/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Is a equal to b? -> " + (a == b));
+        System.out.println("Is a different to b? -> " + (a != b));
+
+        System.out.println("Is a greater than b? -> " + (a > b));
+        System.out.println("Is a less than b? -> " + (a < b));
+        System.out.println("Is a greater than or equal to b? -> " + (a >= b));
+        System.out.println("Is a less than or equal to b? -> " + (a <= b));
+    }
+}
