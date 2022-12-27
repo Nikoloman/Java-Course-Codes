53885a3c1263a00af4a0a3c0e4667551471dcc23
Nikoloman <hectorvel52@gmail.com>
2022-12-27 13:54:55 -0600

    A little practice about Logic operations and stuff

--- If_Statement/src/Main.java
+++ If_Statement/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Reads in console
         Scanner in = new Scanner(System.in);
 
         System.out.println("Write a number");
