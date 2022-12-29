dfb6d4ecb02ba301ed21201b733330e239e43253
Nikoloman <hectorvel52@gmail.com>
2022-12-29 12:54:13 -0600

    A little practice about the syntax of Arrays

---
+++ Arrays/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        //Arrays of 1 dimension
+        String androidVersions[] = new String[17];
+        String days[] = new String[7];
+
+        //Array of 2 dimensions
+        String [][] cities = new String[4][2];
+        /*
+         * +------------------------------+
+         * |  Country    |  City          |
+         * --------------------------------
+         * | México      | CDMX           |
+         * | México      | Guadalajara    |
+         * | Colombia    | Bogotá         |
+         * | Colombia    | Medellín       |
+         * +------------------------------+
+         * */
+
+        //Array of 3 dimensions
+        int numbers[][][] = new int[2][2][2];
+        /*
+         *      +--------+
+         *     /        /|
+         *    /        / |
+         *   +--------+  |
+         *   |        |  |
+         *   |        |  +
+         *   |        | /
+         *   |        |/
+         *   +--------+
+         * */
+
+        //Array of 4 dimensions
+        int numbers4d[][][][] = new int[3][3][3][3];
+    }
+}
