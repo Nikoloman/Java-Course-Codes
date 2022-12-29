8442e2940f2d5212b207ec712b2798a50a5ff8f0
Nikoloman <hectorvel52@gmail.com>
2022-12-29 13:40:28 -0600

    Adding some values to some arrays

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -33,5 +33,19 @@ public class Main {
 
         //Array of 4 dimensions
         int numbers4d[][][][] = new int[3][3][3][3];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        cities[0][0] = "México";
+        cities[0][1] = "CDMX";
+        cities[1][0] = "México";
+        cities[1][1] = "Guadalajara";
+        cities[2][0] = "Colombia";
+        cities[2][1] = "Bogotá";
+        cities[3][0] = "Colombia";
+        cities[3][1] = "Medellín";
     }
 }
